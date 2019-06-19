454e9cfacc351119cfbcf593934a1f045f2bfb13
Mike <bavdysmyh@ukr.net>
2019-06-19 23:26:33 +0300

    zookeeper mutex

---
+++ src/Mutexes/ZookeeperMutex.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Foxtech package.
+ *
+ * (c) Mykhailo Bavdys <bavdysmyh@ukr.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Foxtech\Mutexes;
+
+use Foxtech\AbstractMutex;
+use Foxtech\MutexInterface;
+use Zookeeper;
+
+/**
+ * Class PdoMutex
+ * @package Foxtech
+ *
+ * @author foxtech <foxtech12@gmail.com>
+ *
+ * @property Zookeeper $handler Zookeeper Handler
+ */
+class ZookeeperMutex extends AbstractMutex implements MutexInterface
+{
+    /**
+     * {@inheritdoc}
+     * @see MutexInterface::acquire()
+     */
+    public function acquire(): void
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     * @see MutexInterface::release()
+     */
+    public function release(): void
+    {
+        
+    }
+}
