17d393d9cc6c9c0da7c0e9eb399c5feabf2bc9d2
Mike <bavdysmyh@ukr.net>
2019-06-20 21:48:46 +0300

    add flock interpret

--- src/Mutexes/FlockMutex.php
+++ src/Mutexes/FlockMutex.php
@@ -11,7 +11,9 @@
 
 namespace Foxtech\Mutexes;
 
+use Foxtech\AbstractMutex;
 use Foxtech\MutexInterface;
+use Exception;
 
 /**
  * Class FlockMutex
@@ -19,15 +21,52 @@ use Foxtech\MutexInterface;
  *
  * @author foxtech <foxtech12@gmail.com>
  */
-class FlockMutex implements MutexInterface
+class FlockMutex extends AbstractMutex implements MutexInterface
 {
+    private $lockHandle;
+
     /**
      * {@inheritdoc}
      * @see MutexInterface::acquire()
      */
-    public function acquire(): void
+    public function acquire(string $lockPath = null): void
     {
+        if ($this->lockHandle) {
+            return;
+        }
 
+        $fileName = sprintf('%s/sf.%s.%s.lock',
+            $lockPath ?? sys_get_temp_dir(),
+            preg_replace('/[^a-z0-9\._-]+/i', '-', $this->name),
+            strtr(substr(base64_encode(hash('sha256', $this->name, true)), 0, 7), '/', '_')
+        );
+
+        set_error_handler(function ($type, $msg) use (&$error) { $error = $msg; });
+
+        if (!$handle = fopen($fileName, 'r+') ?: fopen($fileName, 'r')) {
+            if ($handle = fopen($fileName, 'x')) {
+                chmod($fileName, 0666);
+            } elseif (!$handle = fopen($fileName, 'r+') ?: fopen($fileName, 'r')) {
+                usleep(100); // Give some time for chmod() to complete
+                $handle = fopen($fileName, 'r+') ?: fopen($fileName, 'r');
+            }
+        }
+
+        restore_error_handler();
+
+        if (!$handle) {
+            // TODO: change customize exception
+            throw new Exception($error, 0, null);
+        }
+        // On Windows, even if PHP doc says the contrary, LOCK_NB works, see
+        // https://bugs.php.net/54129
+        if (!flock($handle, LOCK_EX | LOCK_NB)) {
+            fclose($handle);
+            // TODO: change customize exception
+            throw new Exception();
+        }
+
+        $this->lockHandle = $handle;
     }
 
     /**
@@ -36,6 +75,11 @@ class FlockMutex implements MutexInterface
      */
     public function release(): void
     {
+        if (!$this->lockHandle) {
+            return;
+        }
 
+        flock($this->lockHandle, LOCK_UN | LOCK_NB);
+        fclose($this->lockHandle);
     }
 }
