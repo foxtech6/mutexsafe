50bc43c9c88d380d5dac9ed83d48b4242d1714c0
mike <bavdysmyh@ukr.net>
2020-10-09 23:07:26 +0300

    fix flock mutex

--- src/Mutexes/FlockMutex.php
+++ src/Mutexes/FlockMutex.php
@@ -42,20 +42,17 @@ class FlockMutex extends AbstractMutex implements MutexInterface
 
         $fileName = sprintf('%s/sf.%s.%s.lock',
             $lockPath ?? sys_get_temp_dir(),
-            preg_replace('/[^a-z0-9\._-]+/i', '-', $this->name),
+            $this->name,
             strtr(
                 substr(base64_encode(hash('sha256', $this->name, true)), 0, 7), '/', '_'
             )
         );
 
-        set_error_handler(function ($type, $msg) use (&$error) { $error = $msg; });
+        set_error_handler(function (int $type, string $msg) use (&$error) { $error = $msg; });
 
         if (!$handle = fopen($fileName, 'r+') ?: fopen($fileName, 'r')) {
             if ($handle = fopen($fileName, 'x')) {
                 chmod($fileName, 0666);
-            } elseif (!$handle = fopen($fileName, 'r+') ?: fopen($fileName, 'r')) {
-                usleep(100); // Give some time for chmod() to complete
-                $handle = fopen($fileName, 'r+') ?: fopen($fileName, 'r');
             }
         }
 
@@ -63,10 +60,9 @@ class FlockMutex extends AbstractMutex implements MutexInterface
 
         if (!$handle) {
             // TODO: change customize exception
-            throw new Exception($error, 0, null);
+            throw new Exception();
         }
-        // On Windows, even if PHP doc says the contrary, LOCK_NB works, see
-        // https://bugs.php.net/54129
+
         if (!flock($handle, LOCK_EX | LOCK_NB)) {
             fclose($handle);
             // TODO: change customize exception
